a30cd5860699d0fcf7e98cd0a6bbfda5e295917d
Gustavo Peixoto <henriquegustavo2010@gmail.com>
2020-04-24 15:18:12 -0300

    Versao 0

---
+++ Exerc_Resol_Meth_Abs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exerc_Resol_Meth_Abs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
